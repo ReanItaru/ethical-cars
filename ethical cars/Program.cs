bde30e0597f68cc7c542e7ecd0b80dcf30881b11
ReanItaru <uratinear@gmail.com>
2016-12-10 18:32:38 -0700

    1

--- ethical cars/Program.cs
+++ ethical cars/Program.cs
@@ -10,6 +10,36 @@ namespace ethical_cars
     {
         static void Main(string[] args)
         {
+            //defined elsewhere in this theoretical program (human life == 4,294,967,295 animal lives) //largest size of a uint
+            //property is less important than life and the car itself is least important property
+            if (lossOfLife == (2 >= group && leastLossTotal))
+            {
+                else if (Illegal == true)
+                {
+                    Illegal(group) = Die();
+                }
+                else if (Age.group[0] != Age.group[1])
+                {
+                    Age(group).Largest = Die();
+                }
+                else if (Gender.group[0] != Gender.group[1])
+                {
+                    Gender(group).Male = Die();
+                }
+                else
+                {
+                    Driving(group).Passengers = Die();
+                }
+            }
+            else if (group[any] == leastLossTotal)
+            {
+                foreach (group g in accident)
+                {
+                    if (group[g] == leastLossTotal)
+                        group[g] = Marked;
+                }
+                Marked(group) = Die();
+            }
         }
     }
 }
